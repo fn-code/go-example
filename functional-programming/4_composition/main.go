0aaf0b162257ee93b02498905509ae6b431e9510
Ludin Nento <ludyyn@gmail.com>
2019-05-26 18:03:45 +0800

    Update example

--- functional-programming/4_composition/main.go
+++ functional-programming/4_composition/main.go
@@ -5,14 +5,6 @@ import (
 	"strings"
 )
 
-type strFunc func(string) string
-
-func Compose(f, g strFunc) strFunc {
-	return func(s string) string {
-		return g(f(s))
-	}
-}
-
 func main() {
 	f1 := func(s string) string {
 		return fmt.Sprintf("your hobby is %v", s)
@@ -22,4 +14,41 @@ func main() {
 	}
 	cmps := Compose(f1, g1)
 	fmt.Println(cmps("programming"))
+
+	yCombo := combi(fibFuncFunc)
+	fmt.Println("r(r)(x) in anonymous function in yCombinator is a Lambda Expression:")
+	fmt.Println("> yCombo(5):", yCombo(5))
+}
+
+type strFunc func(string) string
+
+func Compose(f, g strFunc) strFunc {
+	return func(s string) string {
+		return g(f(s))
+	}
+}
+
+type FC func(int) int
+type FCFC func(FC) FC
+type RecursiveFC func(RecursiveFC) FC
+
+func fibFuncFunc(f FC) FC {
+	return func(x int) int {
+		if x == 0 {
+			return 0
+		} else if x <= 2 {
+			return 1
+		} else {
+			return f(x-2) + f(x-1)
+		}
+	}
+}
+
+func combi(f FCFC) FC {
+	r := func(a RecursiveFC) FC {
+		return f(func(x int) int {
+			return a(a)(x)
+		})
+	}
+	return r(r)
 }
